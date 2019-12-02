6e5304e19501c41af95041c66447648d5c2faef5
wats0ns <quentin.maire@protonmail.ch>
2019-12-02 10:02:03 +0100

    Config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pypfm',
+      version='1.0',
+      description='Python pfm files reader',
+      author='Wats0ns',
+      author_email='Wats0ns@github.com',
+      url='https://github.com/Wats0ns/pypfm',
+      packages=['pypfm'],
+     )
