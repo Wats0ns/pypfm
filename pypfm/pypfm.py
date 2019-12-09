3a25624177190f1e161e5270c099710d7f44323b
wats0ns <quentin.maire@protonmail.ch>
2019-12-09 17:15:59 +0100

    Added compression

--- pypfm/pypfm.py
+++ pypfm/pypfm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import re
+from pyzfp import compress, decompress
 reg = re.compile(r'^(\d+)\s(\d+)\s$')
 
 HEADER_SIZE = len('PF\n')
@@ -9,7 +10,7 @@ BENDIAN_SIZE = len('{:f}\n'.format(1))
 
 
 class PFMLoader:
-    def __init__(self, dim=None, color=True, endian='<'):
+    def __init__(self, dim=None, color=True, endian='<', compress=False):
         self.width, self.height = dim
         self.color = color
         self.endian = endian
@@ -19,11 +20,16 @@ class PFMLoader:
         else:
             self.data_size = HEADER_SIZE + self.dim_size + BENDIAN_SIZE
         self.shape = (self.height, self.width, 3) if self.color else (self.height, self.width, 1)
-
+        self.compress = compress
         print(self.data_size)
 
     def load_pfm(self, path):
-        data = np.fromfile(path, self.endian + 'f', offset=self.data_size)
+        data = np.fromfile(path, offset=self.data_size, dtype=np.uint8 if self.compress else self.endian + 'f')
+        # return data
+        # print('At load: ', self.shape, data.dtype, np.ascontiguousarray(data, dtype=np.dtype('uint8').shape))
+        if self.compress:
+            data = decompress(np.ascontiguousarray(data, dtype=np.uint8), self.shape, np.dtype('float32'), tolerance=0.5, parallel=False)
+            return data
         data = np.reshape(data, self.shape)
         return data
 
@@ -35,9 +41,13 @@ class PFMLoader:
         endian = image.dtype.byteorder
         if endian == '<' or endian == '=' and sys.byteorder == 'little':
             scale = -scale
+        shape = image.shape
+        if self.compress:
+            image = np.ascontiguousarray(compress(image, tolerance=0.5, parallel=False), dtype=np.uint8)
         with open(path, 'w') as file:
             file.write('PF\n' if color else 'Pf\n')
-            file.write('%d %d\n' % (image.shape[1], image.shape[0]))
+            file.write('%d %d\n' % (shape[1], shape[0]))
             file.write('%f\n' % scale)
             image.tofile(file)
+        return image
 
--- tests/test_pfm.py
+++ tests/test_pfm.py
@@ -2,13 +2,35 @@ import numpy as np
 import sys
 sys.path.append('.')
 from pypfm import PFMLoader
-
+from pyzfp import decompress
 
 def test_save_and_load():
     shape = (2, 3)
     data = np.random.rand(shape[0], shape[1], 1).astype('float32')
-    loader = PFMLoader((shape[1], shape[0]), False)
+    loader = PFMLoader((shape[1], shape[0]), False, compress=False)
 
     loader.save_pfm('test.pfm', data)
     res = loader.load_pfm('test.pfm')
     assert np.array_equal(res, data)
+
+
+def test_save_and_load_with_compression():
+    import pypfm
+    print(pypfm.__version__)
+    shape = (2, 3)
+    data = np.random.rand(shape[0], shape[1], 1).astype('float32')
+    loader = PFMLoader((shape[1], shape[0]), False, compress=True)
+
+    saved = loader.save_pfm('test_.pfm', data)
+    res = loader.load_pfm('test_.pfm')
+    # assert np.array_equal(saved, res)
+    res = decompress(np.ascontiguousarray(saved, dtype=np.uint8), data.shape, np.dtype('float32'), tolerance=0.5,
+               parallel=False)
+    error = np.abs((res - data))
+    mean_error = np.mean(error)
+    relative_error = np.mean(error /np.abs(data))
+    print(error)
+    print(res)
+    print(mean_error, relative_error)
+    # q
+    assert relative_error < 0.1
