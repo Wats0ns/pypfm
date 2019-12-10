a608bf512bc8e7c6e31f1977fe4c6e2bcdfddb53
Wats0ns <quentin.maire@protonmail.ch>
2019-12-10 08:13:21 +0100

    Good

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(name='pypfm',
       version='1.0',
@@ -7,4 +8,5 @@ setup(name='pypfm',
       author_email='Wats0ns@github.com',
       url='https://github.com/Wats0ns/pypfm',
       packages=['pypfm'],
+      install_requires=['pyzfp', 'numpy']
      )
