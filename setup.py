206d3b360f7dca4a2b2d365ac5e49c989e1b222a
wats0ns <quentin.maire@protonmail.ch>
2019-12-16 11:12:58 +0100

    Trying to publish

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from distutils.core import setup
 from setuptools import find_packages, setup
+import pypandoc
 
+output = pypandoc.convert_file('README.md', 'rst')
+print(output)
 setup(name='pypfm',
-      version='1.4.1',
+      version='1.4.2',
+      long_description=output,
       description='Python pfm files reader',
       author='Wats0ns',
       author_email='Wats0ns@github.com',
       url='https://github.com/Wats0ns/pypfm',
       packages=['pypfm'],
+      license='MIT',
       install_requires=['pyzfp', 'numpy'],
-      include_package_data=True,
+      # include_package_data=True,
+
      )
